04ccbc5e7b4c58975e52d27138a5aaa02f5f1f7d
0siribix <nomail@me.pl>
2022-01-25 23:10:50 -0600

    Try it this way

--- tubelib_addons_0siribix/init.lua
+++ tubelib_addons_0siribix/init.lua
@@ -1,3 +1,6 @@
+#tubelib_addons_0siribix
+
+curFolder = minetest.get_modpath(minetest.get_current_modname())
 if minetest.get_modpath("tubelib_addons1") and tubelib_addons1 then
-	dofile("./vacuum.lua")
+	dofile(curFolder .. "/vacuum.lua")
 end
